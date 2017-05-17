d0006b0cdb01e3b1d391c5ab17ea83759722ec7c
leizhang <zhanglei520vip@163.com>
2017-05-17 22:40:41 +0100

    last version

--- script/controllers/route_main.py
+++ script/controllers/route_main.py
@@ -1,13 +1,13 @@
-from flask import render_template, request,g
-from . import bpmain,bpmain_filter
+from flask import render_template, request, redirect, url_for
+from . import bpmain, bpmain_filter
 from script.services.hpp import hpp_search
 from script.services.sale import sale_search
 import json
 from filter import login_filter
 from script.models.database import db_session
 
-
 bpmain_filter.before_request(login_filter)
+
 
 @bpmain.after_request
 def close_session_after_request(response):
@@ -39,10 +39,26 @@ def location_hpp_details():
         'date': date_flag
     }
 
-    if bexist and poutput and poutput['hpp_analysis']:
+    if bexist and poutput['district_list'] and poutput['district_hpp_analysis']:
         return render_template('/hpp/details.html', qry_str=qry_str, bexist=bexist, ptype=ptype, poutput=poutput)
     else:
         return render_template('hpp/search.html')
+
+
+@bpmain_filter.route('/hpp/history/records')
+def hpp_history_records():
+    return redirect(url_for('bpmain.hpp_search_page'))
+
+
+@bpmain_filter.route('/hpp/report/2017')
+def hpp_report_2017():
+    return redirect(url_for('static', filename='data/housing_market_report_q1_2017.pdf'))
+
+
+@bpmain_filter.route('/hpp/report/2016')
+def hpp_report_2016():
+    return redirect(url_for('static', filename='data/2015-16_EHS_Headline_Report.pdf'))
+
 
 
 @bpmain_filter.route('/sale/area/detail')
