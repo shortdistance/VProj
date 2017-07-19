6f32c8191a54de1b60040ea2e4063edac2794b4e
leizhang <zhanglei520vip@163.com>
2017-07-19 23:42:39 +0100

    1

--- script/models/mongodb.py
+++ script/models/mongodb.py
@@ -1,20 +1,13 @@
 import pymongo
 import json
 
+
 def insert_db(mongodb_url, data):
     client = pymongo.MongoClient(mongodb_url,
-                         connectTimeoutMS=30000,
-                         socketTimeoutMS=None,
-                         socketKeepAlive=True)
+                                 connectTimeoutMS=30000,
+                                 socketTimeoutMS=None,
+                                 socketKeepAlive=True)
 
-    db = client.projdb
+    db = client.get_default_database()
 
-    if not isinstance(data, dict):
-        try:
-            data = json.loads(data)
-        except Exception as e:
-            data = {}
-
-    if data:
-        db.collection.insert_one(data)
-
+    db.my_collection.insert_one(data)
