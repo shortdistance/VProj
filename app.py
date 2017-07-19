7b097e7d75e233c782193a67b084b74ba379fe3f
leizhang <zhanglei520vip@163.com>
2017-07-19 17:17:02 +0100

    The latest version

--- app.py
+++ app.py
@@ -1,9 +1,7 @@
 #!/bin/env python
 from script import create_app
-import os
 
 app = create_app()
 
 if __name__ == '__main__':
-    PORT = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=5001)
+    app.run()
